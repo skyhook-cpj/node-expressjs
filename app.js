4cd03df5113c1d3f6e8fba16c02f9628727fd6e8
砧木 <zhenmu.zyl@alibaba-inc.com>
2019-12-26 16:04:35 +0800

    add test case

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+var server = app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+module.exports = server;
